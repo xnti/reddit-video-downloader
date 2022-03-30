f31536573fdc12ef10e691481f3625c6c007e696
xnti <cinarakaan@gmail.com>
2022-03-30 23:34:21 +0300

    main.cpp arguments control.

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,21 @@
 #include "include/app.h"
 #include "include/json.h"
 
-int main(int argc, char *argv[])
+int main(int argc, const char *argv[])
 {
-    char *video_url = argv[1];
+    if(argc < 2) {
+        printf("\033[1;31mUsage: $ redditvid <url> \033[0m\n");
+        return 1;
+    }
+
+    const char *video_url = argv[1];
+    std::string video_url_str = video_url;
+
+    if(video_url_str.substr(0,4) != "http") {
+        printf("\033[1;31mPlease provide a valid http(s) url. \033[0m\n");
+        return 1;
+    }
+
     xnti::pApp()->run(video_url);
     return 0;
 }
