521a70365024ac4c38974c77647ce96d58d94fc3
xnti <cinarakaan@gmail.com>
2022-06-26 16:21:55 +0300

    nlohmann/json updated.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
-/*     
+/*
  *      author  : xnti (https://github.com/xnti)
  *      license : MIT
  */
-
 
 #include <iostream>
 #include "include/app.h"
@@ -10,16 +9,22 @@
 
 int main(int argc, const char *argv[])
 {
-    if(argc < 2) {
-        printf("\033[1;31mUsage: $ %s <url> \033[0m\n", argv[0]);
+    if (argc < 2)
+    {
+        std::system("clear");
+        printf("\033[1;31m\n[ ! ] Missing argument, please provide a http(s) url. \n\033[0m\n");
+        printf("\033[1;31mUsage: $ %s <url> \n\033[0m\n", argv[0]);
         return 1;
     }
 
     const char *video_url = argv[1];
     std::string video_url_str = video_url;
 
-    if(video_url_str.substr(0,4) != "http") {
-        printf("\033[1;31mPlease provide a valid http(s) url. \033[0m\n");
+    if (video_url_str.substr(0, 4) != "http")
+    {
+        std::system("clear");
+        printf("\033[1;31m\n[ ! ] Please provide a valid http(s) url. \n\033[0m\n");
+        printf("\033[1;31mUsage: $ %s <url> \n\033[0m\n", argv[0]);
         return 1;
     }
 
